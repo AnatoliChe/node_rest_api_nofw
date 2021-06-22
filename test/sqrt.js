06b6c6ec293b5338c6e296a4c2988fa0773cbe1f
“AnatoliChe” <anatoli.che@gmail.com>
2021-06-22 15:37:18 +0300

    add eslint

--- test/sqrt.js
+++ test/sqrt.js
@@ -3,26 +3,23 @@ process.env.NODE_ENV = 'test'
 const chai = require('chai')
 const chaiHttp = require('chai-http')
 const server = require('../server')
+// eslint-disable-next-line no-unused-vars
 const should = chai.should()
 
 chai.use(chaiHttp)
 
-describe('math', () => {
+describe('math', function () {
   // Consts
   const id = '3'
   const result = 9
   const successCode = 200
-  const math = {
-    id: `${id}`,
-    result: `${result}`
-  }
 
-  describe('/GET sqrt', () => {
-    it('it should GET a math ', done => {
+  describe('/GET sqrt', function () {
+    it('it should GET a math ', function (done) {
       chai.request(server)
         .get(`/api/sqrt/${id}`)
         .end((err, res) => {
-          res.should.have.status(200)
+          res.should.have.status(successCode)
           res.body.should.have.property('result').eql(result)
           done()
         })
